7b61803f2c53af7baa3fa26e059e3d8aa97fb78c
Ravi Kumar <ravikumar.off66@gmail.com>
2024-01-22 02:37:42 +0530

    todo

--- src/App.js
+++ src/App.js
@@ -1,33 +1,5 @@
-import React, { useState } from 'react';
 import './App.css';
-import Keypad from './keypad';
 
-function App() {
-  let [input, update] = useState('');
-
-  function onClickEvent(value) {
-    update(input + value);
-  }
-
-  function funckey(value) {
-    if (value === 'c') {
-      update('');
-    } else {
-      let outputval = eval(input);
-      update(outputval);
-    }
-  }
-  return (
-    <React.Fragment>
-      <div className="container">
-        <h1>Calculater App</h1>
-        <div className="calculater">
-          <input type="text" value={input} className="output" />
-          <Keypad onClickEvent={onClickEvent} funckey={funckey}></Keypad>
-        </div>
-      </div>
-    </React.Fragment>
-  );
-}
+function App() {}
 
 export default App;
